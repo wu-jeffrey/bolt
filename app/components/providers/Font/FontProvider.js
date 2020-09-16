f977e4ab6350ec244aac69445cfe1104765c53c4
wu-jeffrey <j387wu@edu.uwaterloo.ca>
2020-09-15 23:51:08 -0400

    Add roboto font

--- app/components/providers/Font/FontProvider.js
+++ app/components/providers/Font/FontProvider.js
@@ -8,6 +8,20 @@ import {
   Quicksand_600SemiBold,
   Quicksand_700Bold,
 } from "@expo-google-fonts/quicksand";
+import {
+  Roboto_100Thin,
+  Roboto_100Thin_Italic,
+  Roboto_300Light,
+  Roboto_300Light_Italic,
+  Roboto_400Regular,
+  Roboto_400Regular_Italic,
+  Roboto_500Medium,
+  Roboto_500Medium_Italic,
+  Roboto_700Bold,
+  Roboto_700Bold_Italic,
+  Roboto_900Black,
+  Roboto_900Black_Italic,
+} from "@expo-google-fonts/roboto";
 
 export function FontProvider(props) {
   let [fontsLoaded] = useFonts({
@@ -16,6 +30,18 @@ export function FontProvider(props) {
     Quicksand_500Medium,
     Quicksand_600SemiBold,
     Quicksand_700Bold,
+    Roboto_100Thin,
+    Roboto_100Thin_Italic,
+    Roboto_300Light,
+    Roboto_300Light_Italic,
+    Roboto_400Regular,
+    Roboto_400Regular_Italic,
+    Roboto_500Medium,
+    Roboto_500Medium_Italic,
+    Roboto_700Bold,
+    Roboto_700Bold_Italic,
+    Roboto_900Black,
+    Roboto_900Black_Italic,
   });
 
   if (!fontsLoaded) {
--- app/components/ui/Typography.js
+++ app/components/ui/Typography.js
@@ -2,9 +2,36 @@ import React from "react";
 import { StyleSheet, Text } from "react-native";
 
 export function Typography(props) {
-  return <Text style={styles.text}>{props.children}</Text>;
+  const font = props?.font || "quicksand";
+  const weight = props?.weight || "regular";
+  const size = props?.size || 12;
+  const color = props?.color || "black";
+
+  return (
+    <Text style={{ ...textStyle(font, weight, size, color), ...props.style }}>
+      {props.children}
+    </Text>
+  );
 }
 
-const styles = StyleSheet.create({
-  text: { fontFamily: "Quicksand_400Regular" },
-});
+function textStyle(font, weight, size, color) {
+  return {
+    fontFamily: fontFamilies[`${font}_${weight}`],
+    fontSize: size,
+    color: color,
+  };
+}
+
+const fontFamilies = {
+  quicksand_light: "Quicksand_300Light",
+  quicksand_regular: "Quicksand_400Regular",
+  quicksand_medium: "Quicksand_500Medium",
+  quicksand_semibold: "Quicksand_600SemiBold",
+  quicksand_bold: "Quicksand_700Bold",
+  roboto_thin: "Roboto_100Thin",
+  roboto_light: "Roboto_300Light",
+  roboto_regular: "Roboto_400Regular",
+  roboto_medium: "Roboto_500Medium",
+  roboto_bold: "Roboto_700Bold",
+  roboto_black: "Roboto_900Black",
+};
